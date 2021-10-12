5062f3374896a57230d2508e8eddaaa5febd88cd
OteroSantiago <91058589+OteroSantiago@users.noreply.github.com>
2021-10-12 20:51:09 +0200

    Initial commit

---
+++ src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author Santiago
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte numeroByte = 5;
+        System.out.println(numeroByte);
+        System.out.println("Valor mínimo del tipo byte: "+Byte.MIN_VALUE);
+        System.out.println("Valor máximo del tipo byte: "+Byte.MAX_VALUE);
+        System.out.println("");
+        short numeroShort = 30000;
+        System.out.println(numeroShort);
+        System.out.println("Valor mínimo del tipo short: "+Short.MIN_VALUE);
+        System.out.println("Valor máximo del tipo short: "+Short.MAX_VALUE);
+        System.out.println("");
+        int numeroInt = 30000;
+        System.out.println(numeroInt);
+        System.out.println("Valor mínimo del tipo int: "+Integer.MIN_VALUE);
+        System.out.println("Valor mínimo del tipo int: "+Integer.MAX_VALUE);
+        System.out.println("");
+        long numeroLong = 920357808;
+        System.out.println(numeroLong);
+        System.out.println("Valor mínimo del tipo long: "+Long.MIN_VALUE);
+        System.out.println("Valor máximo del tipo long: "+Long.MAX_VALUE);
+        System.out.println("");
+        
+        
+    }
+    
+}
